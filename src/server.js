adbd2bc404c4bff42fdae9c739ab688804a5034d
jkmathanmohan <30907025+jkmathanmohan@users.noreply.github.com>
2020-03-05 20:48:16 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hi This is my Test Docker Web server !!!');
+  res.send('Hi This is My Test Docker Web server !!!');
 });
 
 app.listen(PORT, HOST);
